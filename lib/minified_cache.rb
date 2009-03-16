3e0a58eb828c8d6718e86ba42a1d0fd5edf84909
cornflakesuperstar <cornflakesuperstar@hotmail.com>
2009-03-17 02:01:28 +1100

    implemented.

--- lib/minified_cache.rb
+++ lib/minified_cache.rb
@@ -1,1 +1,19 @@
-# MinifiedCache
+module ActionView::Helpers::AssetTagHelper
+  class AssetCollection
+	  def write_asset_file_contents(joined_asset_path)
+	    FileUtils.mkdir_p(File.dirname(joined_asset_path))
+	    # start monkey-patch
+	    jsmin = File.join(File.dirname(__FILE__), '/jsmin.rb')
+	    tmp = Tempfile.new('src_js')
+	    tmp.print(joined_contents)
+	    tmp.rewind
+	    minified = Tempfile.new('minified')
+	    %x[ruby #{jsmin} < #{tmp.path} > #{minified.path}]
+	    File.open(joined_asset_path, "w+") { |cache| cache.write(minified.read) }
+	    # end monkey-patch
+	    # REPLACED: File.open(joined_asset_path, "w+") { |cache| cache.write(joined_contents) }
+	    mt = latest_mtime
+	    File.utime(mt, mt, joined_asset_path)
+	  end
+	end
+end
